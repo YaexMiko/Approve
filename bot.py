f308eb6de904113f256812ca8f0386b15d245442
Yae Miko <aloksinghmains756@gmail.com>
2025-05-10 01:22:52 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,32 +1,27 @@
 from pyrogram import Client
-from config import API_ID, API_HASH, BOT_TOKEN
+from config import config  # Changed to import the config instance
 
 class Bot(Client):
-
     def __init__(self):
         super().__init__(
-        "vj join request bot",
-         api_id=API_ID,
-         api_hash=API_HASH,
-         bot_token=BOT_TOKEN,
-         plugins=dict(root="plugins"),
-         workers=50,
-         sleep_threshold=10
+            "vj join request bot",
+            api_id=config.get("API_ID"),  # Access through config instance
+            api_hash=config.get("API_HASH"),  # Access through config instance
+            bot_token=config.get("BOT_TOKEN"),  # Access through config instance
+            plugins=dict(root="plugins"),
+            workers=50,
+            sleep_threshold=10
         )
 
-      
     async def start(self):
-            
         await super().start()
         me = await self.get_me()
         self.username = '@' + me.username
-            
         print('Bot Started Powered By @VJ_Botz')
 
-
     async def stop(self, *args):
-
         await super().stop()
         print('Bot Stopped Bye')
 
-Bot().run()
+if __name__ == "__main__":
+    Bot().run()
