d5d56b4891a6afb783b56b158aa29a0288850bb5
Yae Miko <aloksinghmains756@gmail.com>
2025-05-10 01:09:55 +0530

    Update database.py

--- plugins/database.py
+++ plugins/database.py
@@ -1,5 +1,6 @@
 import motor.motor_asyncio
 from config import DB_NAME, DB_URI
+from datetime import datetime
 
 class Database:
     
@@ -7,12 +8,14 @@ class Database:
         self._client = motor.motor_asyncio.AsyncIOMotorClient(uri)
         self.db = self._client[database_name]
         self.col = self.db.users
+        self.groups_col = self.db.groups
 
     def new_user(self, id, name):
         return dict(
             id = id,
             name = name,
             session = None,
+            join_date = datetime.now()
         )
     
     async def add_user(self, id, name):
@@ -38,6 +41,21 @@ class Database:
 
     async def get_session(self, id):
         user = await self.col.find_one({'id': int(id)})
-        return user['session']
+        return user.get('session')
+
+    # New methods for group tracking
+    async def add_group(self, id, title):
+        await self.groups_col.update_one(
+            {'id': int(id)},
+            {'$set': {'id': int(id), 'title': title, 'last_updated': datetime.now()}},
+            upsert=True
+        )
+
+    async def is_group_exist(self, id):
+        group = await self.groups_col.find_one({'id': int(id)})
+        return bool(group)
+
+    async def total_groups_count(self):
+        return await self.groups_col.count_documents({})
 
 db = Database(DB_URI, DB_NAME)
